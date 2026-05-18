56fc3e5a6311d0ad67378877b7b25432e61a6387
Taanveer22 <taanveer469@gmail.com>
2026-05-18 06:17:26 +0600

    alhamdulliah admin job status update operation done

--- src/components/AdminViewApplications.jsx
+++ src/components/AdminViewApplications.jsx
@@ -1,11 +1,34 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import Swal from 'sweetalert2';
 
 const AdminViewApplications = () => {
   const [viewApps, setViewApps] = useState([]);
   // console.log(viewApps);
   const { jobId } = useParams();
   // console.log(jobId);
+
+  const handleUpdateStatus = (e, id) => {
+    // console.log(e.target.value, id);
+    const data = {
+      status: e.target.value,
+    };
+
+    fetch(`http://localhost:5000/applications/admin/status/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        if (data.modifiedCount > 0) {
+          Swal.fire('Job status updated successfully');
+        }
+      });
+  };
 
   useEffect(() => {
     fetch(`http://localhost:5000/applications/admin/view/${jobId}`)
@@ -37,7 +60,17 @@ const AdminViewApplications = () => {
                 <th>{index + 1}</th>
                 <td>{appItem.applicant_email}</td>
                 <td>{appItem.job_id}</td>
-                <td>hired</td>
+                <td>
+                  <select
+                    onChange={(e) => handleUpdateStatus(e, appItem._id)}
+                    defaultValue={appItem?.status || 'Update Status'}
+                    className="select"
+                  >
+                    <option>Hired</option>
+                    <option>Rejected</option>
+                    <option>Under Review</option>
+                  </select>
+                </td>
               </tr>
             ))}
           </tbody>
